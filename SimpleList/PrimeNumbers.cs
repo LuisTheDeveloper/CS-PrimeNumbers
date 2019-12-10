99194413af5d87d377a3829308143e158ad6057e
Luis Magalhaes <sprintlmaga@gmail.com>
2019-12-10 15:36:54 +0000

    Added a class to encapsulate the main code.

---
+++ SimpleList/PrimeNumbers.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleList
+{
+    public class PrimeNumbers
+    {
+        public PrimeNumbers()
+        {
+
+        }
+        public int MaxNumber { get; set; }
+        
+
+        public void DisplayPrimeNumbers()
+        {
+            int wTemp = 0;
+
+
+            IList<int> intList = new List<int>();
+
+            var PrimeNumbers = new List<int>();
+            var NonPrimeNumbers = new List<int>();
+
+            try
+            {
+                var MaxNum = MaxNumber;
+
+                Console.WriteLine($"Displaying Prime Numbers from 2 to {MaxNumber}");
+
+                //Let's add all the integers numbers to the List
+                for (int i = 2; i <= MaxNumber; i++)
+                {
+                    if (!PrimeNumbers.Contains(i) & !NonPrimeNumbers.Contains(i))
+                    {
+                        PrimeNumbers.Add(i);
+                    }
+
+                    for (int j = i; j <= MaxNumber; j++)
+                    {
+                        if (j % i != 0)
+                        {
+                            if ((!NonPrimeNumbers.Contains(j) & !PrimeNumbers.Contains(j)))
+                            {
+                                PrimeNumbers.Add(j);
+                            }
+
+                        }
+                        else
+                        {
+                            if (PrimeNumbers.Contains(j) & j != i)
+                            {
+                                PrimeNumbers.Remove(j);
+                                NonPrimeNumbers.Add(j);
+                            }
+                            else
+                            {
+                                if ((j % i == 0) & (j != i) & !NonPrimeNumbers.Contains(j))
+                                {
+                                    NonPrimeNumbers.Add(j);
+                                }
+                            }
+                        }
+
+                    }
+                }
+
+                Console.WriteLine();
+                Console.WriteLine($"There are {PrimeNumbers.Count} prime numbers");
+                Console.WriteLine();
+
+                do
+                {
+                    Console.Write(PrimeNumbers[wTemp]);
+                    Console.Write(" ");
+                    wTemp = ++wTemp;
+                }
+                while (wTemp < PrimeNumbers.Count);
+                
+
+            }
+            //catch (FormatException)
+            catch (SystemException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine("User Input Invalid");
+                Console.ReadLine();
+            }
+        }
+    }
+}
--- SimpleList/Program.cs
+++ SimpleList/Program.cs
@@ -1,87 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 
-public class SamplesArrayList
+namespace SimpleList
 {
 
-    public static void Main(string[] args)
+    class Program
     {
-        int wTemp = 0;
-        
-        IList<int> intList = new List<int>();
-
-        var PrimeNumbers = new List<int>();
-        var NonPrimeNumbers = new List<int>();
-
-        Console.WriteLine("Algorithm to find the Prime Numbers starting at 2");
-        Console.WriteLine(" ");
-
-        Console.WriteLine("What is the maximum number?");
-        var UserNumber = Console.ReadLine();
-
-
-        try
+        public static void Main(string[] args)
         {
-            var MaxNumber = Int32.Parse(UserNumber);
-            Console.WriteLine($"Displaying Prime Numbers from 2 to {MaxNumber}");
+            Console.WriteLine("Algorithm to find the Prime Numbers starting at 2");
+            Console.WriteLine(" ");
 
-            //Let's add all the integers numbers to the List
-            for (int i = 2; i <= MaxNumber; i++)
-            {
-                if (!PrimeNumbers.Contains(i) & !NonPrimeNumbers.Contains(i))
-                {
-                    PrimeNumbers.Add(i);
-                }  
+            PrimeNumbers objPN = new PrimeNumbers();
 
-                for (int j = i; j <= MaxNumber; j++)
-                {
-                    if (j % i != 0)
-                    {
-                        if ((!NonPrimeNumbers.Contains(j) & !PrimeNumbers.Contains(j)))
-                        {
-                            PrimeNumbers.Add(j);
-                        }
-                        
-                    }
-                    else
-                    {
-                        if (PrimeNumbers.Contains(j) & j!=i)
-                        {
-                            PrimeNumbers.Remove(j);
-                            NonPrimeNumbers.Add(j);
-                        }
-                        else
-                        {
-                            if ((j % i == 0) & (j != i) & !NonPrimeNumbers.Contains(j))
-                            {
-                                NonPrimeNumbers.Add(j);
-                            }
-                        }
-                    }
-                }
-            }
+            Console.WriteLine("What is the maximum number?");
+            var UserNumber = Console.ReadLine();
 
-            Console.WriteLine();
-            Console.WriteLine(PrimeNumbers.Count);
-            Console.WriteLine();
-
-            do
-            {
-                Console.WriteLine(PrimeNumbers[wTemp]);
-                wTemp = ++wTemp;
-            }
-            while (wTemp < PrimeNumbers.Count);
-
-
-        }
-        //catch (FormatException)
-        catch (SystemException e)
-        {
-            Console.WriteLine(e.Message);
-            Console.WriteLine("User Input Invalid");
+            objPN.MaxNumber =  Int32.Parse(UserNumber);
+            objPN.DisplayPrimeNumbers();
             Console.ReadLine();
+
         }
-
     }
-
 }
